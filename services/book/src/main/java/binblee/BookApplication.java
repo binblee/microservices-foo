cd87cef02176f5d441a23ef234a90ca0f6aee9db
Li Bin <binblee@users.noreply.github.com>
2015-11-23 15:20:51 +0800

    eureka client for book service

--- services/book/src/main/java/binblee/BookApplication.java
+++ services/book/src/main/java/binblee/BookApplication.java
@@ -2,8 +2,10 @@ package binblee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookApplication {
 
     public static void main(String[] args) {
