5aa6badbd5bdb5cbd0cfc36f1ac35a2fde4f8613
Li Bin <binblee@users.noreply.github.com>
2015-11-23 13:20:01 +0800

    naming as EurekaServer

--- discovery/src/main/java/binblee/EurekaServer.java
+++ discovery/src/main/java/binblee/EurekaServer.java
@@ -2,14 +2,13 @@ package binblee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaServer.class, args);
     }
 }
