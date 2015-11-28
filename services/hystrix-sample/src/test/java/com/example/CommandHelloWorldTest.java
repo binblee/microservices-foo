b25bbb63f2cb14d926878a16b01208c4b26c862c
Li Bin <binblee@users.noreply.github.com>
2015-11-28 13:37:56 +0800

    asynchronos test

--- services/hystrix-sample/src/test/java/com/example/CommandHelloWorldTest.java
+++ services/hystrix-sample/src/test/java/com/example/CommandHelloWorldTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.concurrent.Future;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -15,8 +17,16 @@ import static org.junit.Assert.assertEquals;
 @SpringApplicationConfiguration(classes = HystrixApplication.class)
 public class CommandHelloWorldTest {
 
+    private static final String RESULT = "Hello World";
+    private static final String PARAM = "World";
+
     @Test
     public void testSynchronous(){
-        assertEquals("Hello World",new CommandHelloWorld("World").execute());
+        assertEquals(RESULT,new CommandHelloWorld(PARAM).execute());
+    }
+
+    public void testAsynchronos() throws Exception {
+        Future<String> fs = new CommandHelloWorld(PARAM).queue();
+        assertEquals(RESULT,fs.get());
     }
 }
