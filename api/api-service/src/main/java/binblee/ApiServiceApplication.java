c641e81c2781dc4a3fbfa029724527788876d15d
Li Bin <binblee@users.noreply.github.com>
2015-12-06 21:59:53 +0800

    add api-service initial code

---
+++ api/api-service/src/main/java/binblee/ApiServiceApplication.java
@@ -0,0 +1,12 @@
+package binblee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApplication.class, args);
+    }
+}
