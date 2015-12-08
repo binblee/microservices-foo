75e531445bed9d597e92610edb1355e59cb57617
Li Bin <binblee@users.noreply.github.com>
2015-12-08 22:08:38 +0800

    add spring-boot-starter-web dependency, got context load successfully in test

--- api/authserver/src/test/java/binblee/AuthserverApplicationTests.java
+++ api/authserver/src/test/java/binblee/AuthserverApplicationTests.java
@@ -2,11 +2,15 @@ package binblee;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AuthserverApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class AuthserverApplicationTests {
 
 	@Test
