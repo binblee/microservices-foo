8909bbd22ef8a1fc66e11c9342bf4d2b448385d1
Li Bin <binblee@users.noreply.github.com>
2015-12-08 22:42:42 +0800

    test unauthorized access

--- api/authserver/README.md
+++ api/authserver/README.md
@@ -3,3 +3,7 @@
 
 ### Get latest version of spring oauth
 [spring-security-oauth](http://projects.spring.io/spring-security-oauth/)
+
+### Test authserver
+
+Use TestRestTemplate to invoke URL, otherwise you will get 401 exception.
--- api/authserver/src/test/java/binblee/AuthserverApplicationTests.java
+++ api/authserver/src/test/java/binblee/AuthserverApplicationTests.java
@@ -2,10 +2,18 @@ package binblee;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AuthserverApplication.class)
@@ -13,8 +21,39 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @IntegrationTest("server.port:0")
 public class AuthserverApplicationTests {
 
+	@Value("${local.server.port}")
+	private int port;
+
+	private RestTemplate restTemplate = new TestRestTemplate();
+
 	@Test
-	public void contextLoads() {
+	public void homePageProtected(){
+		ResponseEntity<String> response = restTemplate.getForEntity(
+			"http://localhost:" + port + "/uaa/", String.class
+		);
+		assertEquals(HttpStatus.UNAUTHORIZED,response.getStatusCode());
+		String auth = response.getHeaders().getFirst("WWW-Authenticate");
+		System.out.println("auth:" + auth);
+		assertTrue("Wrong header: " + auth, auth.startsWith("Bearer realm=\""));
 	}
+
+	@Test
+	public void userEndpointProtected() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:"
+				+ port + "/uaa/user", String.class);
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+		String auth = response.getHeaders().getFirst("WWW-Authenticate");
+		assertTrue("Wrong header: " + auth, auth.startsWith("Bearer realm=\""));
+	}
+
+	@Test
+	public void authorizationRedirects() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:"
+				+ port + "/uaa/oauth/authorize", String.class);
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+		String auth = response.getHeaders().getFirst("WWW-Authenticate");
+		assertTrue("Wrong header: " + auth, auth.startsWith("Bearer realm=\""));
+	}
+
 
 }
