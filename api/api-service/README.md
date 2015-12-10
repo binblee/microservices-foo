7f67770e0b2ace7553357b27e7c72fec217c86b3
Li Bin <binblee@users.noreply.github.com>
2015-12-10 21:46:22 +0800

    fix @OAuth2Resource annonation problem

---
+++ api/api-service/README.md
@@ -0,0 +1,5 @@
+# API Service
+
+### OAuth2Resource
+
+@OAuth2Resource available in spring boot 1.2.2, not in 1.3.0.
--- api/api-service/src/main/java/binblee/ApiServiceApplication.java
+++ api/api-service/src/main/java/binblee/ApiServiceApplication.java
@@ -2,8 +2,10 @@ package binblee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 @SpringBootApplication
+@EnableOAuth2Resource
 public class ApiServiceApplication {
 
     public static void main(String[] args) {
