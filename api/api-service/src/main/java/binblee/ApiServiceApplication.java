d75a4c638f30e954e38bdf484a865ab4af3fc753
Li Bin <binblee@users.noreply.github.com>
2015-12-11 15:46:53 +0800

    api-service eureka client

--- api/api-service/src/main/java/binblee/ApiServiceApplication.java
+++ api/api-service/src/main/java/binblee/ApiServiceApplication.java
@@ -2,10 +2,12 @@ package binblee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 @SpringBootApplication
 @EnableOAuth2Resource
+@EnableEurekaClient
 public class ApiServiceApplication {
 
     public static void main(String[] args) {
--- api/api-service/src/main/java/binblee/service/BookReviewApiService.java
+++ api/api-service/src/main/java/binblee/service/BookReviewApiService.java
@@ -1,8 +1,12 @@
 package binblee.service;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by libin on 12/10/15.
  */
+
+@RestController
 public class BookReviewApiService {
     //TODO add code for access book review service
 }
