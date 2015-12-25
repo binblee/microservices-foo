8338f603f2bc89d5bad82d61518fb4e5f06de4a6
Li Bin <binblee@users.noreply.github.com>
2015-12-25 10:21:18 +0800

    append reviews with random value to show roundrobin effects

--- services/review/src/main/java/binblee/ReviewController.java
+++ services/review/src/main/java/binblee/ReviewController.java
@@ -1,6 +1,7 @@
 package binblee;
 
 import binblee.model.Review;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,23 +16,25 @@ import java.util.*;
 @RestController
 public class ReviewController {
 
-    private Map<String,Review> reviews;
+
+    @Value("${eureka.instance.instanceId}")
+    private String randomValue;
 
     public ReviewController() {
-        reviews = new HashMap<String,Review>();
-        List<String> isbnList = Arrays.asList("1","2","3","4");
-        for( String isbn : isbnList ){
-            List<String> comments = Arrays.asList(
-                    "comment 1 for " + isbn,
-                    "comment 2 for " + isbn,
-                    "comment 3 for " + isbn);
-            Review review = new Review(isbn,comments);
-            reviews.put(isbn,review);
-        }
     }
 
     @RequestMapping(value="/review/{isbn}", method = RequestMethod.GET)
     public Review getReview(@PathVariable String isbn){
+        Map<String,Review> reviews = new HashMap<String,Review>();
+        List<String> isbnList = Arrays.asList("1","2","3","4");
+        for( String isbnInList : isbnList ){
+            List<String> comments = Arrays.asList(
+                    randomValue + " comment 1 for " + isbnInList,
+                    randomValue + " comment 2 for " + isbnInList,
+                    randomValue + " comment 3 for " + isbnInList);
+            Review review = new Review(isbn,comments);
+            reviews.put(isbn,review);
+        }
         return reviews.get(isbn);
     }
 }
